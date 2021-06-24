3155cea1b20b1cce992191ec3b6b417d5fa8e430
maguon <>
2021-06-24 16:28:27 +0800

    add purchase_refund

--- util/SystemConst.js
+++ util/SystemConst.js
@@ -5,6 +5,26 @@ const status = { //使用状态
     usable:1//可用
 }
 
+const storageType ={
+    import:1, //入库
+    export:2 //出库
+}
+
+const storageImportType ={
+    purchaseImport:11, //采购入库
+    storageMoveImport:12,//移库入库
+    storageCountImport:13,//盘盈入库
+    orderBackImport:14,//退单入库
+    innerBackImport:15//内部退料入库
+}
+const storageExportType ={
+    purchaseExport:21,//采购退货出库
+    storageMoveExport:22,//移库出库
+    storageCountExport:23,//盘盈出库
+    orderExport:24,//订单出库
+    innerExport:25//内部领料出库
+}
+
 module.exports = {
     status
 }
